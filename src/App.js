1f89c1811e0f4b8a36cb02f744095ebff8ca41a7
darijavan <darijavan@gmail.com>
2020-03-24 17:31:53 +0300

    add GameBoard to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
     <div className="App">
+      <GameBoard style={{ margin: "20px auto" }}>Hello World!</GameBoard>
     </div>
   );
 }
